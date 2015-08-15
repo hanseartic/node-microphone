96fc6ea718d69f5d0bf22c49df363146fd2b8d89
hanseartic <schuette.paul@gmail.com>
2015-08-15 14:37:17 +0200

    Emits events in a simple fashion
    forwards the existing recording-events and additionally emits events on start and stop of capturing

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
 var isMacOrWin = require('os').type() == 'Darwin' || require('os').type().indexOf('Windows') > -1;
 var spawn = require('child_process').spawn
 var PassThrough = require('stream').PassThrough;
 var lame = require('lame');
 
+function Microphone () {
+    EventEmitter.call(this);
+    this.audioStream = new PassThrough;
+    this.infoStream = new PassThrough;
+
+    this.infoStream.on('data', function(data) {
+        this.emit('info',
+                  data);
+    }.bind(this));
+    this.audioStream.on('data', function(data) {
+        this.emit('audio',
+                  {
+                      'stream': this.audioStream,
+                      'buffer': data
+                  });
+    }.bind(this));
+}
+
+util.inherits(Microphone, EventEmitter);
+
 var ps = null;
 
-var audio = new PassThrough;
-var info = new PassThrough;
-
-var start = function(options) {
+Microphone.prototype.startCapture = function(options) {
     options = options || {};
     
     if(ps == null) {
@@ -17,32 +37,31 @@ var start = function(options) {
         : spawn('arecord', ['-D', 'plughw:1,0', '-f', 'dat']);
 
         if(options.mp3output === true) {
-            var encoder = new lame.Encoder( {
+            var encoder = new lame.Encoder({
                 channels: 2,
                 bitDepth: 16,
                 sampleRate: 44100
             });
 
             ps.stdout.pipe(encoder);
-            encoder.pipe(audio);
-            ps.stderr.pipe(info);
 
+            encoder.pipe(this.audioStream);
+            ps.stderr.pipe(this.infoStream);
         } else {
-            ps.stdout.pipe(audio);
-            ps.stderr.pipe(info);
 
+            ps.stdout.pipe(this.audioStream);
+            ps.stderr.pipe(this.infoStream);
         }
     }
+    this.emit('audio-started', this.audioStream);
 };
 
-var stop = function() {
+Microphone.prototype.stopCapture = function() {
     if(ps) {
         ps.kill();
         ps = null;
     }
+    this.emit('audio-stopped');
 };
 
-exports.audioStream = audio;
-exports.infoStream = info;
-exports.startCapture = start;
-exports.stopCapture = stop;
+module.exports = Microphone;
